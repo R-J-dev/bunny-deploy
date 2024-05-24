5086337585f409a6dee9ff6e7cf06dee9f1f2e20
R-J-dev <115353861+R-J-dev@users.noreply.github.com>
2024-05-24 12:06:00 +0200

    fix: use target-directory in file info gathering

--- src/config/transformers.ts
+++ src/config/transformers.ts
@@ -8,3 +8,9 @@ export const transformDirectoryToUploadInput = async (
     throw new Error("process.env.GITHUB_WORKSPACE is undefined");
   return join(process.env.GITHUB_WORKSPACE, directoryToUpload);
 };
+
+export const removeEndSlash = async (input: string) =>
+  input.endsWith("/") ? input.slice(0, input.length - 1) : input;
+
+export const removeBeginSlash = async (input: string) =>
+  input.startsWith("/") ? input.slice(1) : input;

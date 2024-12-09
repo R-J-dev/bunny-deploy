a7cb3f0a9116b14769cf992376e56260d3a2a9b6
R-J-dev <115353861+R-J-dev@users.noreply.github.com>
2024-12-09 12:27:36 +0100

    ci: add Dependabot updates to releases

--- release.config.js
+++ release.config.js
@@ -3,7 +3,22 @@
  */
 const config = {
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          // Default rules
+          { breaking: true, release: "major" },
+          { revert: true, release: "patch" },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          // Custom rule for production dependency updates
+          { type: "build", scope: "deps", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
   ],
